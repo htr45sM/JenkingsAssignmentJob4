71b30bd5138ca797e2fc4232929d0946185a5c50
htr45sM <107580391+htr45sM@users.noreply.github.com>
2022-08-16 14:55:01 -0400

    Update Job.java

--- Job.java
+++ Job.java
@@ -2,7 +2,7 @@ public class Job {
     public static void main(String[] args){
        System.out.println("Displaying Numbers from 1 to 10");
 
-       for (int i = 1; i <= 10; i++){
+       for (int i = 1; i <= 20; i++){
            System.out.println("Number: " + i);
        }
     }
